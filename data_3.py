ab5792a45e99a185620bafef90d041d579b81c77
unknown <you@example.com>
2024-03-23 00:00:20 +0900

    20240322(데이터수집)

--- data_3.py
+++ data_3.py
@@ -3,9 +3,9 @@ import pandas as pd
 df_A = pd.read_csv("boxoffice.csv", sep = ',', encoding = 'utf-8')
 df_B = pd.read_csv("movie.csv", sep = ',', encoding = 'utf-8')
 
-total = pd.merge(df_A,df_B)
+total = pd.merge(df_A, df_B)
 
-total.to_csv("total.csv")
+total.to_csv("box_info.csv")
 
 df = pd.read_csv("total.csv", sep=',')
 print(df)
